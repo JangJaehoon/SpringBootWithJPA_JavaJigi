7103eecba0e00c66a67fdaf126a16f27428f694c
jjhgwx <jjhgwx@gmail.com>
2020-11-06 00:25:27 +0900

    github test

---
+++ src/main/java/com/example/demo/JavajigiSpringWithJpa1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavajigiSpringWithJpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavajigiSpringWithJpa1Application.class, args);
+	}
+
+}
